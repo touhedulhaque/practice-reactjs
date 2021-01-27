f97e66708631bc11df34ee77a176a8a2042a7c45
Md Touhedul Haque <razmbamkt@gmail.com>
2021-01-27 08:16:59 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -25,50 +25,77 @@ import Person2 from './Component/Person2';
 class App extends Component {
   state = {
     books: [
-      { bookName: "1984-2000", writer: "Jeorge" },
-      { bookName: "Romeo and Juliet", writer: "Shakespear" },
-      { bookName: "World war ii", writer: "paulo" }
+      { id: 1, bookName: "1984-2000", writer: "Jeorge" },
+      { id: 2, bookName: "Romeo and Juliet", writer: "Shakespear" },
+      { id: 3, bookName: "World war ii", writer: "paulo" }
     ],
-    other: "i am others"
+    showBooks: true
   };
+  changeWithInputState = (e, index) => {
+    const book = {
+      ...this.state.books[index]
 
-  changeBookState = (newBookName) => {
-    // console.log("gutai")
-    // normally this.state.books[0].bookName="1974"
+    }
+    book.bookName = e.target.value;
+    const books = [...this.state.books];
+    books[index] = book;
     this.setState({
-      books: [
-        { bookName: newBookName, writer: "Jeorge" },
-        { bookName: "love and romance", writer: "Shakespear" },
-        { bookName: "World war Z", writer: "paulo" }
-      ]
+      books: books
     });
   }
 
-  changeWithInputState = (e) => {
+  deleteBookState = (index) => {
+    //const books = this.state.books.slice();
+    // const books = this.state.books.map(item => item)
+    const books = [...this.state.books]
+    books.splice(index, 1)
     this.setState({
-      books: [
-        { bookName: e.target.value, writer: "Jeorge" },
-        { bookName: "love and romance", writer: "Shakespear" },
-        { bookName: "World war Z", writer: "paulo" }
-      ]
-    });
+      books: books
+    })
+  };
+
+  toggleBooks = () => {
+    this.setState({ showBooks: !this.state.showBooks })
   }
   // constructor() {
   //   super();
   //   this.state = {};
   // }
   render() {
-    console.log(this.state)
+
+
+    const booksState = this.state.books;
+
+    let books = null;
+    if (this.state.showBooks) {
+      books = booksState.map((book, index) => {
+        return (
+          <Book
+            bookName={book.bookName}
+            writer={book.writer}
+            delete={() => this.deleteBookState(index)}
+            key={book.id}
+            inputName={(e) => this.changeWithInputState(e, index)}
+          ></Book>
+        )
+      })
+    }
+
+
+    // console.log(booksState)
+    console.log(books)
     return (
       <div className="App">
         <h1>Book List</h1>
         <br></br>
-        <button onClick={() => this.changeBookState("Nineteen eighty four")}>Change State</button>
-        <input type="text" onChange={this.changeWithInputState}></input>
+        <button onClick={this.toggleBooks} style={{ background: "yellow" }} type="">Toogle Books</button>
+        {/* <button onClick={() => this.changeBookState("Nineteen eighty four")}>Change State</button>
+        <input type="text" onChange={this.changeWithInputState}></input> */}
 
+        {/* 
         <Person name="RAZ" age="30" >I am from Bangladesh</Person>
-        <Person2 name="Rifat" age="28" />
-        <Book
+        <Person2 name="Rifat" age="28" /> */}
+        {/* <Book
           bookName={this.state.books[0].bookName}
           writer={this.state.books[0].writer}
           inputName={this.changeWithInputState} ></Book>
@@ -78,7 +105,8 @@ class App extends Component {
         <Book
           bookName={this.state.books[2].bookName}
           writer={this.state.books[2].writer}
-          change={this.changeBookState.bind(this, "Nineteen eighty four 1984-2000")} ></Book>
+          change={this.changeBookState.bind(this, "Nineteen eighty four 1984-2000")} ></Book> */}
+        { books}
 
       </div>
     )
--- src/Component/Book.js
+++ src/Component/Book.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
 const Book = props => {
+
     return (
-        <div>
-            <h3 onClick={props.change}>Book: {props.bookName}</h3>
+        <div style={{
+            border: "1px solid orchid",
+            margin: "20px",
+            padding: "20px",
+            background: "azure"
+        }}>
+            <button onClick={props.delete} type="" style={{ color: "red" }}>Delete</button>
+            <h3>Book: {props.bookName}</h3>
             <h4>Writer Name: {props.writer}</h4>
             <input type="text" onChange={props.inputName} name="" value={props.bookName} />
+            <br />
+            <br />
+
+
         </div>
     );
 };
